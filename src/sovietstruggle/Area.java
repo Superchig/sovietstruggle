79a1adf14b400706e00cadbd6d2e23bf97b98754
Christopher Chang <chiggiechang@gmail.com>
2018-05-22 19:06:22 -0700

    Change Area List to differentiate between Allied and Enemy armies

--- src/sovietstruggle/Area.java
+++ src/sovietstruggle/Area.java
@@ -22,72 +22,97 @@ import java.net.URL;
 public class Area
 {
 
-  private static final int IMAGE_HEIGHT = 86;
-  private String name;
-  private ArrayList<Army> armies;
-  private Icon image;
+	private static final int IMAGE_HEIGHT = 86;
+	private String name;
+	private ArrayList<Army> armies;
+	private Icon image;
+	private Faction playerFaction;
 
-  public Area(String name, String imgPath)
-  {
-    this.name = name;
-    armies = new ArrayList<>();
-    image = scaleImage(imgPath);
-  }
+	public Area(String name, String imgPath, Faction playerFaction)
+	{
+		this.name = name;
+		this.playerFaction = playerFaction;
+		armies = new ArrayList<>();
+		image = scaleImage(imgPath);
+	}
 
-  public void addArmy(Army a)
-  {
-    armies.add(a);
-  }
-	
+	public void addArmy(Army a)
+	{
+		armies.add(a);
+	}
+
 	public void removeArmy(Army a)
 	{
 		armies.remove(a);
 	}
 
-  public String getName()
-  {
-    return name;
-  }
+	public String getName()
+	{
+		return name;
+	}
 
-  public Icon getImage()
-  {
-    return image;
-  }
+	public Icon getImage()
+	{
+		return image;
+	}
 
-  private Icon scaleImage(String imgPath)
-  {
-    try
-    {
-      BufferedImage img;
-      if (imgPath.indexOf("http") == 0)
-        img = ImageIO.read(new URL(imgPath));
-      else
-        img = ImageIO.read(new File(imgPath));
-      return new ImageIcon(img.getScaledInstance(-1, IMAGE_HEIGHT, Image.SCALE_SMOOTH));
-    } catch (IOException e)
-    {
-      e.printStackTrace();
-      System.out.println("Error: Failed to scale image at " + imgPath);
-      return new ImageIcon(imgPath);
-    }
-  }
+	private Icon scaleImage(String imgPath)
+	{
+		try
+		{
+			BufferedImage img;
+			if (imgPath.indexOf("http") == 0)
+			{
+				img = ImageIO.read(new URL(imgPath));
+			} else
+			{
+				img = ImageIO.read(new File(imgPath));
+			}
+			return new ImageIcon(img.getScaledInstance(-1, IMAGE_HEIGHT, Image.SCALE_SMOOTH));
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+			System.out.println("Error: Failed to scale image at " + imgPath);
+			return new ImageIcon(imgPath);
+		}
+	}
 
-  @Override
-  public String toString()
-  {
-    String result = name + TextFormat.spaces(13 - name.length());
+	@Override
+	public String toString()
+	{
+		String result = name + TextFormat.spaces(13 - name.length());
 
-    if (armies.isEmpty())
-    {
-      result += "No Armies";
-    } else if (armies.size() == 1)
-    {
-      result += "1 Army";
-    } else
-    {
-      result += armies.size() + " Armies";
-    }
+		boolean hasAllied = false;
+		for (Army army : armies)
+		{
+			if (army.getController() == playerFaction)
+			{
+				hasAllied = true;
+				break;
+			}
+		}
 
-    return result;
-  }
+		if (hasAllied)
+		{
+			result += "Allied Army"
+							+ TextFormat.spaces("No Allied Army  ".length() - "Allied Army".length());
+		} else
+		{
+			result += "No Allied Army  ";
+		}
+
+		if (!hasAllied && armies.size() == 1)
+		{
+			result += "1 Enemy Army";
+		} else if (!hasAllied && !armies.isEmpty())
+		{
+			result += armies.size() + " Enemy Armies";
+		}
+		else
+		{
+			result += "No Enemy Armies";
+		}
+
+		return result;
+	}
 }
